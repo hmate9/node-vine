76686357f53e1b0121e61b9dd701d0deeea911c6
Mark Stuart <stuartmark@gmail.com>
2013-10-10 23:05:17 -0700

    Clean up dead code.

--- test/test.js
+++ test/test.js
@@ -53,36 +53,3 @@ describe("login", function() {
 		});
 	});
 });
-
-
-/*var vine = require("./lib/vine.js"),
-	username = process.env.VINE_USERNAME,
-	password = process.env.VINE_PASSWORD;
-
-vine.login(username, password, function(err, response) {
-	
-	vine.timeline(function(err, response) {
-		// Works
-	});
-
-	vine.popular(function(err, response) {
-		// Works
-	});
-
-	vine.promoted(function(err, response) {
-		// Works
-	});
-
-	vine.tags("funny", function(err, response) {
-		// Works
-	});
-
-	vine.search("cats", function(err, response) {
-		// Works
-	});
-
-	vine.settings(function(err, response) {
-		// Works
-	});
-});
-*/
