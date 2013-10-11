1b231f8a11a0a5012a25f764b5011996390683e8
Mark Stuart <stuartmark@gmail.com>
2013-10-10 22:25:27 -0700

    Added a couple preliminary mocha tests

--- test.js
+++ test.js
@@ -1,4 +1,61 @@
-var vine = require("./lib/vine.js"),
+var assert = require("assert"),
+	vine = require("./lib/vine.js");
+
+describe("login", function() {
+
+	var username = process.env.VINE_USERNAME,
+		password = process.env.VINE_PASSWORD;
+
+	it("should login via POST /users/authenticate", function(next) {
+		vine.login(username, password, function(err, response) {
+			assert.ok(response);
+			next();
+		});
+	});
+
+	it("should fail if no username or password is specified", function(next) {
+		vine.login(null, null, function(err, response) {
+			assert.ok(err);
+			assert.ok(!response);
+			next();
+		});
+	});
+
+	it("should fail if only username is specified", function(next) {
+		vine.login(username, null, function(err, response) {
+			assert.ok(err);
+			assert.ok(!response);
+			next();
+		});
+	});
+
+	it("should fail if only password is specified", function(next) {
+		vine.login(null, password, function(err, response) {
+			assert.ok(err);
+			assert.ok(!response);
+			next();
+		});
+	});
+
+	it("should fail if wrong username is entered", function(next) {
+		vine.login("aSasdfjaiosdfja!asdfasDafsasdf", password, function(err, response) {
+			assert.ok(err);
+			assert.ok(!response);
+			next();
+		});
+	});
+
+	it("should fail if wrong password is entered", function(next) {
+		vine.login(username, "asdf!!f1jf1Qsdfasd@`casf", function(err, response) {
+			assert.ok(err);
+			assert.ok(!response);
+			next();
+		});
+	});
+});
+
+
+/*var vine = require("./lib/vine.js"),
 	username = process.env.VINE_USERNAME,
 	password = process.env.VINE_PASSWORD;
 
@@ -28,3 +85,4 @@ vine.login(username, password, function(err, response) {
 		// Works
 	});
 });
+*/
