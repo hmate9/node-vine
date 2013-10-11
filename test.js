3f828bdd3d857a699e1796e506b6478fcc728df5
Mark Stuart <stuartmark@gmail.com>
2013-10-10 21:56:09 -0700

    Added more APIs

--- test.js
+++ test.js
@@ -4,7 +4,27 @@ var vine = require("./lib/vine.js"),
 
 vine.login(username, password, function(err, response) {
 	
+	vine.timeline(function(err, response) {
+		// Works
+	});
+
 	vine.popular(function(err, response) {
+		// Works
+	});
+
+	vine.promoted(function(err, response) {
+		// Works
+	});
+
+	vine.tags("funny", function(err, response) {
+		// Works
+	});
+
+	vine.search("cats", function(err, response) {
+		// Works
+	});
+
+	vine.settings(function(err, response) {
 		// Works
 	});
 });
